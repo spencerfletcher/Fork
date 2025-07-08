7272a6695a8212eccde17295af330e7efeaa0366
Spencer Fletcher <44955744+cidarid@users.noreply.github.com>
2025-07-08 14:18:53 -0700

    Fixing indentation for <textarea>

--- src/routes/new/+page.svelte
+++ src/routes/new/+page.svelte
@@ -83,8 +83,7 @@
 				id="ingredients"
 				rows="6"
 				class="mt-1 block w-full rounded-md border-gray-300 shadow-sm focus:border-green-500 focus:ring-green-500"
-			>
-			</textarea>
+			></textarea>
 		</div>
 
 		<div>
