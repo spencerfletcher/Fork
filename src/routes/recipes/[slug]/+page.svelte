c0e0aabb6d0ec361e6092faa8691578f5a1e4660
Spencer Fletcher <44955744+cidarid@users.noreply.github.com>
2025-07-07 17:34:40 -0700

    Adding clock to recipe page

--- src/routes/recipes/[slug]/+page.svelte
+++ src/routes/recipes/[slug]/+page.svelte
@@ -1,11 +1,23 @@
 <script lang="ts">
+	import ClockSvg from '$lib/components/ClockSvg.svelte';
+	import type { Recipe } from '$lib/types';
 	let { data } = $props();
+	let recipe: Recipe = data.recipe;
 </script>
 
-<h1>{data.recipe.title}</h1>
-<p>{data.recipe.description}</p>
+<h1>{recipe.title}</h1>
+<p>{recipe.description}</p>
 <ul class="list-disc pl-5">
-	{#each data.recipe.ingredients as ingredient}
+	{#each recipe.ingredients as ingredient}
 		<li>{ingredient}</li>
 	{/each}
 </ul>
+
+<div class="flex items-center space-x-4 pt-3 text-sm text-gray-500">
+	{#if recipe.cookTimeMinutes}
+		<div class="flex items-center gap-1.5">
+			<ClockSvg />
+			<span>{recipe.cookTimeMinutes} min</span>
+		</div>
+	{/if}
+</div>
