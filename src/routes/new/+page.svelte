f8b5308bfddc5196e6d7f4cb7ee0bf16285cad84
Spencer Fletcher <44955744+cidarid@users.noreply.github.com>
2025-07-08 14:30:23 -0700

    Removing other indent

--- src/routes/new/+page.svelte
+++ src/routes/new/+page.svelte
@@ -27,8 +27,7 @@
 				id="description"
 				rows="3"
 				class="mt-1 block w-full rounded-md border-gray-300 shadow-sm focus:border-green-500 focus:ring-green-500"
-			>
-			</textarea>
+			></textarea>
 		</div>
 
 		<div>
