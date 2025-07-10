605dffb4b4759afe8978e87369a6f5f86a0d6a5d
Spencer Fletcher <44955744+cidarid@users.noreply.github.com>
2025-07-10 16:45:32 -0700

    Distinguishing public and private recipes

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -7,10 +7,25 @@
 
 <div class="p-4 sm:p-6 lg:p-8">
 	<div class="container mx-auto">
-		<h1 class="text-3xl font-bold text-gray-900">All Recipes</h1>
+		{#if data.session?.user}
+			<h1 class="text-3xl font-bold text-gray-900">Your Recipes</h1>
+			<div class="mt-2 grid grid-cols-1 gap-6 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4">
+				{#if data.recipes.filter((recipe) => recipe.userId === data.session?.user.id).length > 0}
+					{#each data.recipes.filter((recipe) => recipe.userId === data.session?.user.id) as recipe (recipe.id)}
+						<RecipeCard {recipe} />
+					{/each}
+				{:else}
+					<p class="col-span-full text-gray-500">
+						You have no recipes yet. Start by
+						<a href="/new" class="text-blue-500 hover:underline">adding one!</a>
+					</p>
+				{/if}
+			</div>
+		{/if}
 
-		<div class="mt-8 grid grid-cols-1 gap-6 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4">
-			{#each data.recipes as recipe (recipe.id)}
+		<h1 class="text-3xl font-bold text-gray-900">Public Recipes</h1>
+		<div class="mt-2 grid grid-cols-1 gap-6 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4">
+			{#each data.recipes.filter((recipe) => recipe.userId !== data.session?.user.id) as recipe (recipe.id)}
 				<RecipeCard {recipe} />
 			{/each}
 		</div>
