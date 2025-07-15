7142ef4a9cdf4875c944456c494f30b8fe0884d9
Spencer Fletcher <44955744+cidarid@users.noreply.github.com>
2025-07-15 01:02:51 -0700

    User can now add a rating

--- src/routes/recipes/new/+page.svelte
+++ src/routes/recipes/new/+page.svelte
@@ -55,6 +55,19 @@
 		</div>
 
 		<div>
+			<label for="rating" class="block text-sm font-medium text-gray-700">Rating (out of 5)</label>
+			<input
+				type="number"
+				name="rating"
+				id="rating"
+				min="1"
+				max="5"
+				step="0.1"
+				class="mt-1 block w-full rounded-md border-gray-300 shadow-sm focus:border-green-500 focus:ring-green-500"
+			/>
+		</div>
+
+		<div>
 			<label for="imageUrl" class="block text-sm font-medium text-gray-700">Image URL</label>
 			<input
 				type="url"
