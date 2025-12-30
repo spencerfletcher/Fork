5d05ac1ab48ca57a4cfb73fb02ce3b041b3fcef4
Spencer Fletcher <44955744+cidarid@users.noreply.github.com>
2025-12-30 16:15:51 -0600

    Changing grid spacing

--- src/lib/components/RecipeGrid.svelte
+++ src/lib/components/RecipeGrid.svelte
@@ -16,7 +16,7 @@
 		<p class="text-xl text-gray-500">No recipes found. Try a different search.</p>
 	</div>
 {:else}
-	<div class="grid grid-cols-1 gap-x-8 gap-y-12 md:grid-cols-2 lg:grid-cols-3">
+	<div class="grid grid-cols-1 gap-x-8 gap-y-4 md:grid-cols-2 lg:grid-cols-3">
 		{#each recipes as recipe (recipe.id)}
 			<RecipeCard {recipe} />
 		{/each}
